a44588136b078d9007764a8d02165fd9ce2c2394
Renze Nicolai <renze@rnplus.nl>
2024-06-01 23:46:55 +0200

    More testing

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <pax_gfx.h>
 #include <pax_codecs.h>
 #include <SDL2/SDL.h>
+#include "fonts/chakrapetchmedium.h"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 480
@@ -12,6 +13,15 @@ pax_buf_t fb;
 SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Texture* texture;
+
+// Colors
+pax_col_t color_background = 0xFFEEEAEE;
+pax_col_t color_primary    = 0xFF01BC99;
+pax_col_t color_secondary  = 0xFFFFCF53;
+pax_col_t color_tertiary   = 0xFFFF017F;
+pax_col_t color_quaternary = 0xFF340132;
+
+// PNG assets
 
 typedef enum asset_index {
     ASSET_LOGO,
@@ -23,6 +33,7 @@ typedef enum asset_index {
     ASSET_BADGEHUB_ICON,
     ASSET_SETTINGS_ICON,
     ASSET_POWER_ICON,
+    ASSET_TEMPLATE,
     ASSET_LAST
 } asset_index_t;
 
@@ -42,6 +53,7 @@ bool initialize_asset(asset_index_t index, const char* file_path) {
 bool initialize_assets() {
     if (!initialize_asset(ASSET_LOGO, "resources/badgeteam.png")) return false;
     if (!initialize_asset(ASSET_USER_ICON, "resources/user_icon.png")) return false;
+    //if (!initialize_asset(ASSET_TEMPLATE, "resources/template.png")) return false;
     return true;
 }
 
@@ -173,16 +185,82 @@ int main() {
     printf("Linux graphics test\n");
     pax_buf_init(&fb, NULL, 800, 480, PAX_BUF_16_565RGB);
     pax_buf_reversed(&fb, false);
-    pax_background(&fb, pax_col_rgb(0, 255, 0));
+
+    // Fill the buffer with the background color
+    pax_background(&fb, color_background);
+
+    //pax_draw_image(&fb, &assets[ASSET_TEMPLATE], 0, 0);
+
+    // Footer test
+    int footer_height      = 32;
+    int footer_vmarign     = 7;
+    int footer_hmarign     = 20;
+    int footer_text_height = 16;
+    int footer_text_hmarign = 20;
+    pax_font_t* footer_text_font = &chakrapetchmedium;
+
+    int footer_box_height = footer_height + (footer_vmarign * 2);
+    pax_draw_line(&fb, color_quaternary, footer_hmarign, fb.height - footer_box_height, fb.width - footer_hmarign, fb.height - footer_box_height);
+    pax_right_text(&fb, color_quaternary, footer_text_font, footer_text_height, fb.width - footer_hmarign - footer_text_hmarign, fb.height - footer_box_height + (footer_box_height - footer_text_height) / 2, "↑ / ↓ / ← / → Navigate   🅰 Start");
+
+
+    // Font test
+    /*pax_draw_text(&fb, color_quaternary, pax_font_sky, footer_text_height, footer_hmarign + footer_text_hmarign,  (footer_vmarign + footer_text_height) * 1, "Test test test 🅰🅱🅷🅼🆂🅴↑↓←→⤓");
+    pax_draw_text(&fb, color_quaternary, pax_font_sky_mono, footer_text_height, footer_hmarign + footer_text_hmarign,  (footer_vmarign + footer_text_height) * 2, "Test test test 🅰🅱🅷🅼🆂🅴↑↓←→⤓");
+    pax_draw_text(&fb, color_quaternary, pax_font_marker, footer_text_height, footer_hmarign + footer_text_hmarign,  (footer_vmarign + footer_text_height) * 3, "Test test test 🅰🅱🅷🅼🆂🅴↑↓←→⤓");
+    pax_draw_text(&fb, color_quaternary, pax_font_saira_condensed, footer_text_height, footer_hmarign + footer_text_hmarign,  (footer_vmarign + footer_text_height) * 4, "Test test test 🅰🅱🅷🅼🆂🅴↑↓←→⤓");
+    pax_draw_text(&fb, color_quaternary, pax_font_saira_regular, footer_text_height, footer_hmarign + footer_text_hmarign,  (footer_vmarign + footer_text_height) * 5, "Test test test 🅰🅱🅷🅼🆂🅴↑↓←→⤓");
+    pax_draw_text(&fb, color_quaternary, &chakrapetchmedium, footer_text_height, footer_hmarign + footer_text_hmarign,  (footer_vmarign + footer_text_height) * 6, "Test test test 🅰🅱🅷🅼🆂🅴↑↓←→⤓");*/
+
+    int circle_count = 6;
+    int circle_spacing = 68;
+    int circle_offset = (fb.width - circle_spacing * (circle_count - 1)) / 2;
+
+    for (int i = 0; i < circle_count; i++) {
+        pax_draw_circle(&fb, 0xFFFFFFFF, circle_offset + circle_spacing * i, 375, 28);
+        pax_outline_circle(&fb, 0xFFE4E4E4, circle_offset + circle_spacing * i, 375, 28);
+    }
+
+    int square_count = 5;
+    int square_voffset = 116;
+    int square_hoffset = 62;
+    int square_size = 170;
+    int square_selected = 0;
+    int square_spacing = 1;
+    int square_marign = 2;
+
+    pax_col_t square_colors[] = {
+        0xFFFFFFFF,
+        color_secondary,
+        color_tertiary,
+        color_quaternary,
+        color_primary,
+    };
+
+    for (int i = 0; i < square_count; i++) {
+        if (i == square_selected) {
+            pax_draw_rect(&fb, color_primary, square_hoffset + (square_size + square_spacing) * i, square_voffset, square_size, square_size);
+        }
+        pax_draw_rect(&fb, square_colors[i], square_hoffset + (square_size + square_spacing) * i + square_marign, square_voffset + square_marign, square_size - square_marign * 2, square_size - square_marign * 2);
+    }
+
+    int title_text_voffset = square_voffset - 32;
+    int title_text_hoffset = square_hoffset - 2;
+    int title_text_height = 20;
+    pax_font_t* title_text_font = &chakrapetchmedium;
+
+    pax_draw_text(&fb, color_primary, title_text_font, title_text_height, title_text_hoffset, title_text_voffset, "Example Application Title");
+
+    pax_right_text(&fb, color_quaternary, title_text_font, 20, fb.width - 40, 32, "23:37 :D");
+
 
     // Pre-render background
     pax_buf_t background;
     pax_buf_init(&background, NULL, 800, 480, PAX_BUF_16_565RGB);
-    draw_test(&background);
+    //draw_test(&background);
+    //pax_draw_image(&fb, &background, 0, 0); // Copy background to framebuffer
 
-    pax_draw_image(&fb, &background, 0, 0); // Copy background to framebuffer
-
-    pax_draw_image(&fb, &assets[ASSET_LOGO], (fb.width - assets[ASSET_LOGO].width) / 2, (fb.height - assets[ASSET_LOGO].height) / 2); // Logo
+    //pax_draw_image(&fb, &assets[ASSET_LOGO], (fb.width - assets[ASSET_LOGO].width) / 2, (fb.height - assets[ASSET_LOGO].height) / 2); // Logo
 
     //draw_menu(&fb, fb.width / 4, fb.height / 4, fb.width / 2, fb.height / 2);
     
